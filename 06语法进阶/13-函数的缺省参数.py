f17d316852b93f2531d8742c2387d8798cca5c4d
wenfei-alibaba <xuwenfei2020@aliyun.com>
2021-03-22 21:04:01 +0800

    06语法进阶

---
+++ 06语法进阶/13-函数的缺省参数.py
@@ -0,0 +1,7 @@
+gl_list=[2,1,3]
+# 默认按照升序排序 - 可能会多！
+# gl_list.sort()
+
+# 如果需要降序排序，需要执行reverse参数
+gl_list.sort()
+print(gl_list)
---
+++ 06语法进阶/6-全局变量的位置.py
@@ -0,0 +1,14 @@
+num = 10
+title = "黑马程序员"
+name = "小明"
+
+
+def demo():
+    print("%d" % num)
+    print("%s" % title)
+    print("%s" % name)
+    # 再定义一个全局变量
+    # title = "黑马程序员"
+
+
+demo()
